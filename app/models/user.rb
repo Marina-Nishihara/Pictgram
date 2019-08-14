a2a08e2e92b9ec1186a01b0052f765e960d41d0d
Marina Nishihara <thillana2480xoxo@yahoo.co.jp>
2019-08-14 11:55:50 +0000

    課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   PW_REGEX = /\A(?=.?[a-z])(?=.?\d)[a-z\d]{8,32}\z/i
   validates :password, presence: true, format: { with: PW_REGEX }
   
+  email_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :email, presence: true, format: { with: email_REGEX }
+  
+  validates :name, length: { maximum: 15 }
+  
 end
